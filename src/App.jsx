c71757b2f41e0e7535378a733842bb815a1eb92a
kalyan yatam <kalyanyatam09@gmail.com>
2024-10-18 19:12:08 +0530

    second

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import About from './components/About';
 import Technologies from './components/Technologies';
 import Programming from './components/Programming';
 import Projects from './components/Projects';
-import Contact from './components/contact';
+import Contact from './components/Contact';
 
 
 const App = () => {
